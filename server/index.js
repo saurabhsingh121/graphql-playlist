d8e36c155b1734618972d6f35836c195b3a2b493
Saurabh Singh <saurabhsingh121@gmail.com>
2020-07-23 17:15:29 +0530

    integrated mongodb database

--- server/index.js
+++ server/index.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
-
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+mongoose.connect(
+  `mongodb://${DB_USERNAME}:${DB_PASSWORD}@ds123614.mlab.com:23614/gql-ninja`
+);
+mongoose.connection.once("open", () => {
+  console.log("Connected to database");
+});
 // bind express with graphql
 app.use(
   "/graphql",
