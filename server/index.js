c7fbf735a1f54825c80260896cb2377ce2d987d7
Saurabh Singh <saurabhsingh121@gmail.com>
2020-07-23 14:04:03 +0530

    changed library version and GraphQLID support

--- server/index.js
+++ server/index.js
@@ -1,15 +1,18 @@
 const express = require("express");
-const { graphqlHTTP } = require("express-graphql");
+const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
 
 const app = express();
+
+// bind express with graphql
 app.use(
   "/graphql",
   graphqlHTTP({
-    schema: schema,
+    schema,
+    graphiql: true,
   })
 );
-console.log(graphqlHTTP);
+
 app.listen(4000, () => {
-  console.log("Now listening for request on port 4000");
+  console.log("now listening for requests on port 4000");
 });
