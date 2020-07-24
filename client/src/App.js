9c2a66464def21d0058d045725682dfc62edd51a
Saurabh Singh <saurabhsingh121@gmail.com>
2020-07-24 14:14:55 +0530

    bookList component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import BookList from "./components/BookList";
 
 function App() {
   return (
     <div id="main">
       <h1>Saurabh's Reading List</h1>
+      <BookList />
     </div>
   );
 }
