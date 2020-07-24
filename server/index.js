aa02bde31ac8f13f560eadc10755d0483634a54c
Saurabh Singh <saurabhsingh121@gmail.com>
2020-07-24 19:22:39 +0530

    making queries from react

--- server/index.js
+++ server/index.js
@@ -2,9 +2,13 @@ const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
 const mongoose = require("mongoose");
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
+
+//allow cross origin request
+app.use(cors());
 const DB_USERNAME = process.env.DB_USERNAME;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 mongoose.connect(
